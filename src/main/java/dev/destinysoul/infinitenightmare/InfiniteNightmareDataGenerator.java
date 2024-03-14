f412b601bac910a8f19e75797aea01e3095c0067
EarthlyEric <earthlyeric6@gmail.com>
2024-03-14 22:44:47 +0800

    init commit

---
+++ src/main/java/dev/destinysoul/infinitenightmare/InfiniteNightmareDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.destinysoul.infinitenightmare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfiniteNightmareDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
