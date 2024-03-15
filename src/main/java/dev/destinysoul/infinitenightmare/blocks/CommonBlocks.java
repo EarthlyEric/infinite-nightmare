563249c9eb005a70668dc96062ec19843de2c2d8
EarthlyEric <earthlyeric6@gmail.com>
2024-03-15 23:28:14 +0800

    add CommonBlocks.java

---
+++ src/main/java/dev/destinysoul/infinitenightmare/blocks/CommonBlocks.java
@@ -0,0 +1,32 @@
+package dev.destinysoul.infinitenightmare.blocks;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import dev.destinysoul.infinitenightmare.InfiniteNightmare;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class CommonBlocks {
+    public static final Block UNDEFINED_BLOCK = registerBlock("undefined_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(InfiniteNightmare.ModID, name), block);
+    }
+
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, new Identifier(InfiniteNightmare.ModID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerCommonBlocks() {
+        InfiniteNightmare.LOGGER.info("Registering commonBlocks for " + InfiniteNightmare.ModID);
+    }
+}
